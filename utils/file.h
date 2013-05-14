0ec9a894652382c30919adb577ed3d7a0394486f
Delos Chang <delosfchang@gmail.com>
2013-05-14 15:48:07 -0400

    refactoring filepath

---
+++ utils/file.h
@@ -0,0 +1,20 @@
+#ifndef _FILE_H_
+#define _FILE_H_
+
+// *****************Impementation Spec********************************
+// File: file.c
+// This file contains useful information for implementing the indexer:
+// - PROTOTYPES
+
+
+// function PROTOTYPES used by file.c 
+
+// dirScan: scans through the directory and returns files that
+// are numbers and files only
+// e.g. 1, 12, 999 (all files)
+// FILTERED: abc (file), 1 (folder), 1s (file)
+int dirScan(char *dir);
+
+char* createFilepath(char* filepath, char* dir, char* name);
+
+#endif

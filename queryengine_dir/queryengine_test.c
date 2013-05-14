170d621ccf951af2e1e233a00eefa66f314b1f1f
Delos Chang <delosfchang@gmail.com>
2013-05-14 19:53:33 -0400

    saving unit tests

--- queryengine_dir/queryengine_test.c
+++ queryengine_dir/queryengine_test.c
@@ -8,15 +8,18 @@
 //
 //  It uses these files but they are not unit tested in this test harness:
 //
-//  DICTIONARY* InitDictionary();
-//  int make_hash(char* c);
-//  void CleanDictionary(DICTIONARY* dict)
+//   void sanitizeKeywords(char** queryList);
+//   void cleanUpIndex(INVERTED_INDEX* index);
+//   DocumentNode* newDocNode(DocumentNode* docNode, int docId, int page_freq);
+//   void cleanUpQueryList(char** queryList);
+//   
 // 
 //  It tests the following functions:
 //
-//   void DAdd(DICTIONARY* dict, void* data, char* key);
-//   void DRemove(DICTIONARY* dict, char* key);
-//   void* GetDataWithKey(DICTIONARY* dict, char* key);
+//   INVERTED_INDEX* initStructure(INVERTED_INDEX* index);
+//   DocumentNode** intersection(DocumentNode** final, DocumentNode** list,
+//   char** curateWords(char** queryList, char* query);
+//   void rankByFrequency(DocumentNode** saved, int l, int r);
 //
 //  If any of the tests fail it prints status 
 //  If all tests pass it prints status.
@@ -34,10 +37,18 @@
 //  up the environment the code expects when integrated in the real
 //  system.
 //
-//  The test harness dummies out the real hash function and through
-//  use of a variable called hash manipulates where DNODEs are 
-//  inserted into the DICTIONARY. For example, collisions can be controlled. 
+  /*The test harness dummies out the real hash function and through*/
+  /*use of a variable called hash manipulates where DNODEs are */
+  /*inserted into the DICTIONARY. For example, collisions can be controlled. */
 //
+//  The following test cases  (1) are for function:
+//
+//  INVERTED_INDEX* initStructure(INVERTED_INDEX* index);
+//
+//  Test case: TestStructure:1
+//  This test case calls initStructure() and checks to make sure 
+//  search engine can initialize the basic inverted index structure
+//  
 //  The following test cases  (1-3) are for function:
 //
 //  DocumentNode** intersection(DocumentNode** final, DocumentNode** list,
@@ -58,20 +69,25 @@
 //  are not empty. They do not have the same DocumentNode. The intersection
 //  and thus resultant list should be empty 
 //
-//  The following test cases (1-4) for function:
+//  The following test cases (1-2) for function:
 //
-//  void DRemove(DICTIONARY* dict, char* key);
+//   char** curateWords(char** queryList, char* query);
 //
-//  Test case:DREMOVE:1
-//  This test case DAdd() and DRemove()  DNODE from dict for only one element.
+//  Test case: TestCurate:1
+//  This test case calls curateWords() for the condition where the query
+//  are all keywords (no non-alpha characters)
 //
-//  Test case:DREMOVE:2
-//  This test case is tries to see how DRemove() works with multiple nodes for the same 
-//  hash value, the node to be deleted is at the end of the dynamic list.
+//  Test case: TestCurate:2
+//  This test case calls curateWords() for the condition where the query
+//  is empty.
 //
-//  Test case:DREMOVE:3
-//  This test case is tries to see how DRemove() works with multiple nodes of the same hash value, 
-//  the node to be deleted is at the start of the dynamic list.
+//  The following test cases (1) for function:
+//
+//   void rankByFrequency(DocumentNode** saved, int l, int r);
+//
+//  Test case: TestRanking:1
+//  This test case calls rankByFrequency() for the condition the DocumentNode
+//  page frequencies are different and need to be ranked accordingly
 //
 //  Test case:DREMOVE:4
 //  This test case is tries to see how DRemove() works with multiple nodes of the same hash value, 
@@ -159,8 +175,8 @@ void create(){
 }
 
 // Test case: TestStructure:1
-// This test case checks to make sure we can initialize the basic
-// inverted index structure
+// This test case calls initStructure() and checks to make sure 
+// search engine can initialize the basic inverted index structure
 int TestStructure1(){
   START_TEST_CASE;
   LOG("Initializing INVERTED INDEX structure");
@@ -168,9 +184,8 @@ int TestStructure1(){
   indexReload = initStructure(indexReload);
   SHOULD_BE(indexReload != NULL);
 
-  END_TEST_CASE;
-  
   cleanUpIndex(indexReload);
+  END_TEST_CASE;
 }
 
 // Test case: TestArgs:1
@@ -213,13 +228,6 @@ int TestArgs1() {
 int TestANDOp1() {
   START_TEST_CASE;
 
-  create();
-
-  INVERTED_INDEX* indexResult = NULL;
-  indexResult = reloadIndexFromFile("../indexer_dir/index.dat", indexReload);
-  SHOULD_BE(indexResult != NULL);
-  LOG("Reloading INVERTED INDEX structure");
-
   int resultSlot[1000];
   DocumentNode* result[10000];
   BZERO(result, 10000);
@@ -234,11 +242,10 @@ int TestANDOp1() {
   list1[0] = docNode;
 
   intersection(list1, list2, result, resultSlot);
-  SHOULD_BE(result[0] == NULL);
+  SHOULD_BE(result[15] == NULL);
 
   free(docNode);
 
-  cleanUpIndex(indexReload);
   END_TEST_CASE;
 }
 
@@ -248,13 +255,6 @@ int TestANDOp1() {
 // the intersection will NOT be an empty list (same DocumentNode)
 int TestANDOp2() {
   START_TEST_CASE;
-
-  create();
-
-  INVERTED_INDEX* indexResult = NULL;
-  indexResult = reloadIndexFromFile("../indexer_dir/index.dat", indexReload);
-  SHOULD_BE(indexResult != NULL);
-  LOG("Reloading INVERTED INDEX structure");
 
   int resultSlot[1000];
   DocumentNode* result[10000];
@@ -270,15 +270,15 @@ int TestANDOp2() {
   DocumentNode* docNode2 = NULL;
   docNode2 = newDocNode(docNode2, 15, 1);
 
+  list1[0] = docNode;
   list2[0] = docNode2;
 
   intersection(list1, list2, result, resultSlot);
-  SHOULD_BE(result[0] != NULL);
+  SHOULD_BE(result[15] != NULL);
 
   free(docNode);
   free(docNode2);
 
-  cleanUpIndex(indexReload);
   END_TEST_CASE;
 }
 
@@ -310,6 +310,7 @@ int TestANDOp3() {
   DocumentNode* docNode2 = NULL;
   docNode2 = newDocNode(docNode2, 16, 1);
 
+  list1[0] = docNode;
   list2[0] = docNode2;
 
   intersection(list1, list2, result, resultSlot);
@@ -322,9 +323,78 @@ int TestANDOp3() {
   END_TEST_CASE;
 }
 
-// Test case: DADD:2
-// This test case calls DAdd() puts multiple DNODEs on the dict when there is no hash collisions
-// We put multiply elements in dictionary with no collisions.
+// Test case: TestCurate:1
+// This test case calls curateWords() for the condition where the query
+// are all keywords (no non-alpha characters)
+int TestCurate1() {
+  START_TEST_CASE;
+
+  char query[1000] = "dog cat mouse world";
+  sanitize(query);
+
+  char* queryList[1000];
+  curateWords(queryList, query);
+
+  SHOULD_BE(strcmp(queryList[0],"dog") == 0);
+  SHOULD_BE(strcmp(queryList[1],"cat") == 0);
+  SHOULD_BE(strcmp(queryList[2],"mouse") == 0);
+  SHOULD_BE(strcmp(queryList[3],"world") == 0);
+
+  END_TEST_CASE;
+}
+
+// Test case: TestCurate:2
+// This test case calls curateWords() for the condition where the query
+// is empty.
+int TestCurate2() {
+  START_TEST_CASE;
+
+  char query[1000] = "";
+  sanitize(query);
+
+  char* queryList[1000];
+  curateWords(queryList, query);
+
+  SHOULD_BE(strcmp(queryList[0],"") == 0);
+
+  END_TEST_CASE;
+}
+
+// Test case: TestRanking:1
+// This test case calls rankByFrequency() for the condition the DocumentNode
+// page frequencies are different and need to be ranked accordingly
+int TestRanking1() {
+  START_TEST_CASE;
+
+  DocumentNode* list1[1000];
+  BZERO(list1, 1000);
+
+  DocumentNode* docNode = NULL;
+  docNode = newDocNode(docNode, 1, 1);
+
+  DocumentNode* docNode2 = NULL;
+  docNode2 = newDocNode(docNode2, 16, 2);
+
+  DocumentNode* docNode3 = NULL;
+  docNode3 = newDocNode(docNode3, 1, 3);
+
+  DocumentNode* docNode4 = NULL;
+  docNode4 = newDocNode(docNode4, 1, 4);
+
+  list1[0] = docNode4;
+  list1[1] = docNode3;
+  list1[2] = docNode2;
+  list1[3] = docNode;
+
+  rankByFrequency(list1, 0, 3);
+
+  free(docNode);
+  free(docNode2);
+  free(docNode3);
+  free(docNode4);
+
+  END_TEST_CASE;
+}
 
 /*int TestDAdd2() {*/
   /*START_TEST_CASE;*/
@@ -521,17 +591,14 @@ int main(int argc, char** argv) {
   RUN_TEST(TestANDOp1, "AND operator Test case 1");
   RUN_TEST(TestANDOp2, "AND operator Test case 2");
   RUN_TEST(TestANDOp3, "AND operator Test case 3");
-  /*RUN_TEST(TestDAdd2, "DAdd Test case 2");*/
-  /*RUN_TEST(TestDAdd3, "DAdd Test case 3");*/
-  /*RUN_TEST(TestDRemove1, "DRemove Test case 1");*/
-  /*RUN_TEST(TestDRemove2, "DRemove Test case 2");*/
-  /*RUN_TEST(TestDRemove3, "DRemove Test case 3");*/
-  /*RUN_TEST(TestDRemove4, "DRemove Test case 4");*/
-  /*RUN_TEST(TestGetData1, "GetDataWithKey Test case 1");*/
+  RUN_TEST(TestCurate1, "Curate Keywords Test case 1");
+  RUN_TEST(TestCurate2, "Curate Keywords Test case 2");
+  RUN_TEST(TestRanking1, "Quicksort Ranking Test case 1");
+
   if (!cnt) {
     printf("All passed!\n Passed: %d \n", cnt); return 0;
   } else {
-    printf("Some fails!\n Passed: %d \n", cnt"); return 1;
+    printf("Some fails!\n Passed: %d \n", cnt); return 1;
   }
 }
 
